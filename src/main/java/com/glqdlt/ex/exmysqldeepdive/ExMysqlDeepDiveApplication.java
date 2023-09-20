65f6d6202d082e882470832c02206bc4675f6dab
glqdlt <dlfdnd0725@gmail.com>
2023-09-20 10:02:39 +0900

    Init

---
+++ src/main/java/com/glqdlt/ex/exmysqldeepdive/ExMysqlDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.exmysqldeepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExMysqlDeepDiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExMysqlDeepDiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glqdlt/ex/exmysqldeepdive/ExMysqlDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.exmysqldeepdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExMysqlDeepDiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
